deafd2023d5481e27a57b837a32c0bfb28f6fe43
Scott Ross <scott.william.ross2@gmail.com>
2022-02-10 20:21:19 -0500

    finished get_movie_image

--- tmdb.py
+++ tmdb.py
@@ -21,13 +21,24 @@ def get_movie(id):
 
     # convert request to json
     r_json = r.json()
-    name = ""
     desc = (r_json["title"], r_json["tagline"], [d["name"] for d in r_json["genres"]]) 
 
     return desc
 
 def get_movie_image(id):
+    url = BASE_URL + id + "/images"
+
+    # create request
+    r = requests.get(url, params={"api_key": KEY})
+
+    # convert request to json
+    r_json = r.json()
+    #print(r_json["posters"][0])
+    # find the first english language poster being searching through the list of dictionaries from the json
+    poster = list(filter(lambda item: item["iso_639_1"] == "en", r_json["posters"]))[0]
+    image = "https://image.tmdb.org/t/p/w500" + poster["file_path"]
     
+    return image
 
 print(get_movie(movie_id))
-
+print(get_movie_image(movie_id))
