eaea3e55e0f6f85986bece162b80700ef30b7a25
Scott Ross <scott.william.ross2@gmail.com>
2022-02-11 23:08:52 -0500

    added exception handling

--- app.py
+++ app.py
@@ -12,9 +12,22 @@ movies = ["438631", "568124", "634649"]
 @app.route("/")
 def index():
     choice = random.choice(movies)
-    movie = tmdb.get_movie(choice)
-    poster = tmdb.get_movie_image(choice)
-    url = tmdb.search_wiki(movie[0])
+
+    # error handling if something goes wrong with the apis
+    try:
+        movie = tmdb.get_movie(choice)
+    except:
+        move = "Uh-oh, something went wrong, movie info can't be found"
+    
+    try:
+        poster = tmdb.get_movie_image(choice)
+    except:
+        poster = "/static/notfound.jpg"
+
+    try:
+        url = tmdb.search_wiki(movie[0])
+    except:
+        url = "https://en.wikipedia.org/wiki/Lists_of_films"
 
     return flask.render_template("index.html", 
         title=movie[0], 
--- tmdb.py
+++ tmdb.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())  # This is to load your API keys from .env
 
+# api stuff
 TMDB_KEY = os.getenv("TMDB_KEY")
 TMDB_URL = "https://api.themoviedb.org/3/movie/"
 MW_URL = "https://en.wikipedia.org/w/rest.php/v1/search/page"
@@ -20,6 +21,7 @@ def get_movie(id):
 
     # convert request to json
     r_json = r.json()
+    # for loop is used find collect the genres from the list of dictionaries returned by json
     desc = (r_json["title"], r_json["tagline"], [d["name"] for d in r_json["genres"]]) 
 
     return desc
@@ -41,9 +43,10 @@ def get_movie_image(id):
 
 def search_wiki(query):
     url = MW_URL
+    # create request
     r = requests.get(url, params={"q": query + " (film)", "limit": 10})
-    
+    # convert request to json
     r_json = r.json()
-
+    # return wikipedia url at the found id
     return "https://en.wikipedia.org/?curid=" + str(r_json["pages"][0]["id"])
 
